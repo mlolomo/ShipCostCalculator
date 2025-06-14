80b8ebf7a2078172594c982adb0882267485f63a
Mbathio Lo <lombathio356@gmail.com>
2025-06-13 22:29:20 -0400

    Task 1 Completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // class Shipping
+        //main()
+        //num itemPrice
+        //num shippingCost
+        //num totalPrice
+        //output "Enter the price of your item: "
+        //input itemPrice
+        //if itemPrice >= 100 then
+        //shippingCost = 0
+        //output "Shipping is free"
+        //else
+        //shippingCost = itemPrice * 0.02
+        //end if
+        //totalPrice = itemPrice + shippingCost
+        //output "Shipping cost: " + shippingCost
+        //output "Total price: " + totalPrice
+        //return
+        //end class
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double itemPrice = 0;
+        double shippingCost = 0;
+        double totalPrice = 0;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the price of your item: ");
+
+        if (in.hasNextDouble()) {
+            itemPrice = in.nextDouble();
+            in.nextLine();
+
+            if (itemPrice >= 100) {
+                shippingCost = 0;
+                System.out.println("Shipping is free");
+            }
+            else {
+                shippingCost = itemPrice * 0.02;
+            }
+
+            totalPrice = itemPrice + shippingCost;
+
+            System.out.println("Shipping cost: " + shippingCost);
+            System.out.println("Total price: " + totalPrice);
+        } else {
+            String trash = in.nextLine();
+            System.out.println("Invalid input: " + trash);
         }
     }
 }
